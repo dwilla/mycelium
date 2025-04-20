bac8922f64bc7f3bed20ac51f278e08912c52d5b
dwilla <drew@bcalternative.com>
2025-04-20 11:03:08 -0600

    setup basic server to test GCP run

--- main.go
+++ main.go
@@ -1,5 +1,32 @@
 package main
 
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
 func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Printf("warning: assuming default configuration. .env unreadable: %v", err)
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("PORT environment variable is not set")
+	}
+
+	mux := http.NewServeMux()
+
+	server := http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	log.Printf("Running at: http://localhost%v\n", server.Addr)
+	server.ListenAndServe()
 
 }
