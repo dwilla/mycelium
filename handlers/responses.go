0c4f1198da1c4d5dd488a3ad39d6fd79f1ca9f7c
dwilla <drew@bcalternative.com>
2025-04-24 14:01:44 -0600

    skipped returning error on response function

--- handlers/responses.go
+++ handlers/responses.go
@@ -8,12 +8,11 @@ import (
 	datastar "github.com/starfederation/datastar/sdk/go"
 )
 
-func respondWithErrors(w http.ResponseWriter, r *http.Request, frontendMsg string, err error) error {
+func respondWithErrors(w http.ResponseWriter, r *http.Request, frontendMsg string, err error) {
 	sse := datastar.NewSSE(w, r)
 	log.Println(frontendMsg, err)
 	newFragment := fmt.Sprintf(`<div id="errors">%s</div>`, frontendMsg)
 	if err := sse.MergeFragments(newFragment); err != nil {
-		return err
+		log.Print("fragment not merged: ", err)
 	}
-	return nil
 }
